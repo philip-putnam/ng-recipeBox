4df927926ddbd81f7f8b1ed1c3e1432d2d3ef5e9
Charles Peden and Philip Putnam <philip@>
2017-03-28 16:25:53 -0700

    add mock-recipes.ts for data

--- src/app/mock-recipes.ts
+++ src/app/mock-recipes.ts
@@ -1,1 +1,7 @@
 import { Recipe } from './recipe.model';
+
+export const ALBUMS: Recipe[] = [
+  new Recipe('Garlic Bread', 'bread, garlic, butter', 'smear butter and garlic to paste, put on bread, toast'),
+  new Recipe('PBJ', 'bread, etc', 'do the things'),
+  new Recipe('Bouillebaise', 'fish', 'boil it in a pot')
+];
